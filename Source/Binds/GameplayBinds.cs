60f3ae24fd105cff2ae7b5eaecd389116eccd8e6
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-11-18 17:14:03 -0700

    Created Bind Node instances

---
+++ Source/Binds/GameplayBinds.cs
@@ -0,0 +1,31 @@
+﻿using Celeste.Mod.ProgrammaticInput.Nodes;
+
+namespace Celeste.Mod.ProgrammaticInput.Binds
+{
+	public static class GameplayBinds
+	{
+		public static readonly AxisXValue MoveX = new();
+
+		public static readonly AxisYValue MoveY = new();
+
+		public static readonly AxisYValue GliderMoveY = new();
+
+
+
+		public static readonly JoystickDirection Aim = new();
+
+		public static readonly JoystickDirection Feather = new();
+
+
+
+		public static readonly ButtonPress Jump = new();
+
+		public static readonly ButtonPress Grab = new();
+
+		public static readonly ButtonPress Dash = new();
+
+		public static readonly ButtonPress Talk = new();
+
+		public static readonly ButtonPress CrouchDash = new();
+	}
+}
---
+++ Source/Binds/MenuBinds.cs
@@ -0,0 +1,31 @@
+﻿using Celeste.Mod.ProgrammaticInput.Nodes;
+
+namespace Celeste.Mod.ProgrammaticInput.Binds
+{
+	public static class MenuBinds
+	{
+		public static readonly ButtonPress ESC = new();
+
+		public static readonly ButtonPress Pause = new();
+
+		public static readonly ButtonPress MenuLeft = new();
+
+		public static readonly ButtonPress MenuRight = new();
+
+		public static readonly ButtonPress MenuUp = new();
+
+		public static readonly ButtonPress MenuDown = new();
+
+		public static readonly ButtonPress MenuConfirm = new();
+
+		public static readonly ButtonPress MenuCancel = new();
+
+		public static readonly ButtonPress MenuJournal = new();
+
+		public static readonly ButtonPress QuickRestart = new();
+
+
+
+		public static readonly JoystickDirection MountainAim = new();
+	}
+}
