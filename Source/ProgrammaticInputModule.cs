b4a328dddec239709dfa39e2cc3d3445cf275070
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-11-18 17:22:59 -0700

    Added overrides to add Node binds on mod load

--- Source/ProgrammaticInputModule.cs
+++ Source/ProgrammaticInputModule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Celeste.Mod.ProgrammaticInput.Binds;
 
 namespace Celeste.Mod.ProgrammaticInput;
 
@@ -22,8 +23,68 @@ public class ProgrammaticInputModule : EverestModule {
     public override void Load() {
         // TODO: apply any hooks that should always be active
     }
-
+	
     public override void Unload() {
         // TODO: unapply any hooks applied in Load()
     }
+
+	public override void OnInputInitialize()
+	{
+		base.OnInputInitialize();
+
+		Input.ESC.Nodes.Add(MenuBinds.ESC);
+		Input.Pause.Nodes.Add(MenuBinds.Pause);
+		Input.MenuLeft.Nodes.Add(MenuBinds.MenuLeft);
+		Input.MenuRight.Nodes.Add(MenuBinds.MenuRight);
+		Input.MenuUp.Nodes.Add(MenuBinds.MenuUp);
+		Input.MenuDown.Nodes.Add(MenuBinds.MenuDown);
+		Input.MenuConfirm.Nodes.Add(MenuBinds.MenuConfirm);
+		Input.MenuCancel.Nodes.Add(MenuBinds.MenuCancel);
+		Input.MenuJournal.Nodes.Add(MenuBinds.MenuJournal);
+		Input.QuickRestart.Nodes.Add(MenuBinds.QuickRestart);
+
+		Input.MoveX.Nodes.Add(GameplayBinds.MoveX);
+		Input.MoveY.Nodes.Add(GameplayBinds.MoveY);
+		Input.GliderMoveY.Nodes.Add(GameplayBinds.GliderMoveY);
+
+		Input.Aim.Nodes.Add(GameplayBinds.Aim);
+		Input.Feather.Nodes.Add(GameplayBinds.Feather);
+		Input.MountainAim.Nodes.Add(MenuBinds.MountainAim);
+
+		Input.Jump.Nodes.Add(GameplayBinds.Jump);
+		Input.Dash.Nodes.Add(GameplayBinds.Dash);
+		Input.Grab.Nodes.Add(GameplayBinds.Grab);
+		Input.Talk.Nodes.Add(GameplayBinds.Talk);
+		Input.CrouchDash.Nodes.Add(GameplayBinds.CrouchDash);
+	}
+
+	public override void OnInputDeregister()
+	{
+		base.OnInputDeregister();
+
+		Input.ESC.Nodes.Remove(MenuBinds.ESC);
+		Input.Pause.Nodes.Remove(MenuBinds.Pause);
+		Input.MenuLeft.Nodes.Remove(MenuBinds.MenuLeft);
+		Input.MenuRight.Nodes.Remove(MenuBinds.MenuRight);
+		Input.MenuUp.Nodes.Remove(MenuBinds.MenuUp);
+		Input.MenuDown.Nodes.Remove(MenuBinds.MenuDown);
+		Input.MenuConfirm.Nodes.Remove(MenuBinds.MenuConfirm);
+		Input.MenuCancel.Nodes.Remove(MenuBinds.MenuCancel);
+		Input.MenuJournal.Nodes.Remove(MenuBinds.MenuJournal);
+		Input.QuickRestart.Nodes.Remove(MenuBinds.QuickRestart);
+
+		Input.MoveX.Nodes.Remove(GameplayBinds.MoveX);
+		Input.MoveY.Nodes.Remove(GameplayBinds.MoveY);
+		Input.GliderMoveY.Nodes.Remove(GameplayBinds.GliderMoveY);
+
+		Input.Aim.Nodes.Remove(GameplayBinds.Aim);
+		Input.Feather.Nodes.Remove(GameplayBinds.Feather);
+		Input.MountainAim.Nodes.Remove(MenuBinds.MountainAim);
+
+		Input.Jump.Nodes.Remove(GameplayBinds.Jump);
+		Input.Dash.Nodes.Remove(GameplayBinds.Dash);
+		Input.Grab.Nodes.Remove(GameplayBinds.Grab);
+		Input.Talk.Nodes.Remove(GameplayBinds.Talk);
+		Input.CrouchDash.Nodes.Remove(GameplayBinds.CrouchDash);
+	}
 }
